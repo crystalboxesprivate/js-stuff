977c8321a51bab451dfb58a638fcf12f89c57fcc
crystalboxes <ursus95@gmail.com>
2020-02-14 13:43:49 +0200

    Added comments

--- weather-app/server.js
+++ weather-app/server.js
@@ -5,25 +5,27 @@ const app = express()
 const request = require('request')
 
 // weather stuff
-const apiKey = '48802695b0c9ae618e13f79d595297a5'
-
 function makeOpenWatherMapUrl(city) {
+  const apiKey = '48802695b0c9ae618e13f79d595297a5'
   return `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 }
 
 let renderError = (res) => { res.render('index', { weather: null, error: "Try again!" }) }
 
+// set body-parser middleware for requests
 app.use(bodyParser.urlencoded({ extended: true }))
-
+// set ejs for rendering
 app.set('view engine', 'ejs')
+// add 'public folder' which html could find
 app.use(express.static('public'))
-
+// get request for the front page
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null})
+  // render - template name
+  res.render('index', { weather: null, error: null })
 })
-
+// post request from the form
 app.post('/', function (req, res) {
-  request(makeOpenWatherMapUrl(req.body.city), function (err, response, body) {
+  request(/* url= */makeOpenWatherMapUrl(req.body.city), /* callback=*/ function (err, response, body) {
     if (err) {
       renderError(res)
     } else {
@@ -36,7 +38,7 @@ app.post('/', function (req, res) {
     }
   })
 })
-
+// this will initialize the server for the events
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
